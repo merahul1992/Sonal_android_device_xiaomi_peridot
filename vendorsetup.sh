4cee6bf909055109f7eefd1c505374948ce6ece7
merahul1992 <90108415+merahul1992@users.noreply.github.com>
2024-10-07 11:57:32 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,4 @@ git clone https://github.com/xiaomi-peridot/vendor_xiaomi_peridot.git -b udc ven
 git clone https://github.com/xiaomi-peridot/hardware_xiaomi.git -b udc hardware/xiaomi
 git clone https://github.com/xiaomi-peridot/device_xiaomi_peridot-miuicamera.git -b 14 device/xiaomi/peridot-miuicamera
 git clone https://codeberg.org/ramshell688/vendor_xiaomi_peridot-miuicamera.git -b main vendor/xiaomi/peridot-miuicamera
-git clone https://github.com/SonalSingh18/vendor_evolution-priv_keys.git -b 14.0 vendor/lineage-priv/keys
+# git clone https://github.com/SonalSingh18/vendor_evolution-priv_keys.git -b 14.0 vendor/lineage-priv/keys
